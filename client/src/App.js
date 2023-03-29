dc5c2c8765292043d95befddba4a9c14f66567c0
Daniel Fernández <47828494+7yrionLannister@users.noreply.github.com>
2023-03-28 20:08:32 -0500

    Connected front-end and back-end. Authentication must be through server authentication because of the authentication middleware I set, or it has to be modified to use tokens from firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import Home from "./pages/Home";
 import React, { useEffect } from 'react';
 import { login, selectUser } from "./features/userSlice";
+import { load, selectMovies } from './features/movieSlice';
 import { useDispatch, useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import Login from "./pages/Login";
 import { auth } from "./firebase";
-import { load, selectMovies } from "./features/movieSlice";
 
 function App() {
-
-  const movies = useSelector(selectMovies);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-
   useEffect(() => {
     onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
@@ -28,25 +25,26 @@ function App() {
     });
   }, [dispatch]);
 
+  const movies = useSelector(selectMovies);
   useEffect(() => {
-    async function asyncFunc() {
-      let baseUrl = 'https://api.themoviedb.org/3/movie/';
-      let m = [
-        await (await fetch(`${baseUrl}500?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json(),
-        await (await fetch(`${baseUrl}501?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json(),
-        await (await fetch(`${baseUrl}502?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json()
-      ];
-      dispatch(
-        load({
-          movies: m
-        })
-      );
-    }
-    asyncFunc();
+      async function asyncFunc() {
+          let baseUrl = 'https://api.themoviedb.org/3/movie/';
+          let m = [
+              await (await fetch(`${baseUrl}500?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json(),
+              await (await fetch(`${baseUrl}501?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json(),
+              await (await fetch(`${baseUrl}502?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)).json()
+          ];
+          dispatch(
+              load({
+                  movies: m
+              })
+          );
+      }
+      asyncFunc();
   }, [dispatch]);
 
   return (
-    user ? <Home items={movies.movies} /> : <Login />
+    user ? <Home items={movies?.movies} /> : <Login />
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { AssignmentTurnedInOutlined, NotificationsOutlined, FeaturedPlayListOutlined, PeopleAltOutlined, Search, Home, AccountCircle } from '@mui/icons-material'
+import React, { useState } from 'react';
+import { AssignmentTurnedInOutlined, NotificationsOutlined, FeaturedPlayListOutlined, PeopleAltOutlined, Search, Home, Close, ExpandMore } from '@mui/icons-material'
+import { Input, Avatar } from '@mui/joy';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectUser } from '../features/userSlice';
+import axios from 'axios';
+
+import Modal from 'react-responsive-modal';
+import "react-responsive-modal/styles.css";
 
 const Header = () => {
+    const user = useSelector(selectUser);
     const dispatch = useDispatch();
     const handleLogout = () => {
         if (window.confirm('Are you sure you want to logout?')) {
@@ -14,6 +20,32 @@ const Header = () => {
                 alert('Logged out');
             }).catch(err => {
                 alert('Error logging out');
+            });
+        }
+    };
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [description, setDescription] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const handleAddQuestion = async () => {
+        if (description !== '') {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+
+            const body = {
+                description,
+                imageUrl,
+                user
+            };
+
+            await axios.post('/api/questions', body, config).then(res => {
+                alert('Question submitted');
+                window.location.href = '/';
+            }).catch(err => {
+                alert('Error adding question\n');
+                console.log(err);
             });
         }
     };
@@ -43,13 +75,79 @@ const Header = () => {
                 <Search />
                 <input type="text" className='bg-transparent outline-none border-none' placeholder='Search...' />
             </div>
-            <div>
+            <div className='flex items-center ml-[25px]'>
                 <span onClick={handleLogout}>
-                    <AccountCircle />
+                    <Avatar src={user?.photo} />
                 </span>
-                <button className='px-2 py-1 rounded-lg ml-1 bg-orange-300 hover:bg-orange-500'>Add Question</button>
+                <button onClick={() => setIsModalOpen(true)} className='px-2 py-1 rounded-lg ml-1 bg-orange-300 hover:bg-orange-500'>Add Question</button>
+                <Modal
+                    open={isModalOpen}
+                    closeIcon={<Close />}
+                    onClose={() => setIsModalOpen(false)}
+                    closeOnEsc
+                    center
+                    closeOnOverlayClick={false}
+                    styles={{
+                        overlay: {
+                            height: "auto",
+                            width: "auto",
+                        },
+                    }}
+                >
+                    <div className="flex items-center mb-2 border-b-2 border-solid border-gray-800/5 rounded-md">
+                        <h5 className="text-orange-400 text-lg cursor-pointer font-semibold mr-[30px]">Add Question</h5>
+                        <h5 className="text-orange-400 text-lg cursor-pointer font-semibold mr-[30px]">Share Link</h5>
+                    </div>
+                    <div className="flex items-center mt-[30px]">
+                        <Avatar src={user?.photo} className="avatar" />
+                        <div className="flex items-center text-gray-600 p-1 ml-2 bg-white rounded-3xl cursor-pointer">
+                            <PeopleAltOutlined />
+                            <p className="ml-2 text-sm text-gray-700">Public</p>
+                            <ExpandMore />
+                        </div>
+                    </div>
+                    <div className="flex flex-col mt-[30px]">
+                        <Input
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            type=" text"
+                            placeholder="Ask your question.. "
+                        />
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <Input
+                                type="text"
+                                value={imageUrl}
+                                placeholder="Optional: Add Image URL"
+                                onChange={(e) => setImageUrl(e.target.value)}
+                            />
+                            {imageUrl !== "" && (
+                                <img
+                                    style={{
+                                        height: "40vh",
+                                        objectFit: "contain",
+                                    }}
+                                    src={imageUrl}
+                                    alt="displayimage"
+                                />
+                            )}
+                        </div>
+                    </div>
+                    <div className="flex flex-col-reverse mt-2 items-center">
+                        <button className="mt-2 border-none outline-none text-gray-400 text-bold p-2 rounded-3xl cursor-pointer hover:text-red" onClick={() => setIsModalOpen(false)}>
+                            Cancel
+                        </button>
+                        <button onClick={handleAddQuestion} type="submit" className="mt-2 border-none outline-none text-orange-500 text-bold p-2 rounded-3xl cursor-pointer hover:text-orange-800 w-1/2">
+                            Add Question
+                        </button>
+                    </div>
+                </Modal>
             </div>
-        </div>
+        </div >
     )
 }
 
